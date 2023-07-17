3cef0226af2dc057f60d8f62db28dc9ce4e5258d
Kush <94019918+kushpatel7764@users.noreply.github.com>
2023-07-17 10:57:16 -0400

    ChessBoard Ready

--- Chess Engine/Assets/Script/CreateBoard.cs
+++ Chess Engine/Assets/Script/CreateBoard.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class CreateBoard : MonoBehaviour
 {
+    public GameObject chessSpot;
+    int heightPiece = 1;
+    int widthPiece = 1; 
+    int heightBoard = 1*8;
+    int widthBoard = 1*8;
     // Start is called before the first frame update
     void Start()
     {
         
+        for (int i = 0; i < heightBoard; i = i + heightPiece ){
+            for (int k = 0; k < widthBoard; k = k + widthPiece ){
+                
+                if (i % 2 == 0){
+                    if (k % 2 == 0){
+                      chessSpot.GetComponent<SpriteRenderer>().color = Color.white;
+                    }else{
+                      chessSpot.GetComponent<SpriteRenderer>().color = Color.black;
+                    }
+                } else{
+                    if (k % 2 == 0){
+                      chessSpot.GetComponent<SpriteRenderer>().color = Color.black;
+                    }else{
+                      chessSpot.GetComponent<SpriteRenderer>().color = Color.white;
+                    }
+                }
+                
+               
+                Instantiate(chessSpot, new Vector2(k,i), Quaternion.identity, this.transform);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +41,5 @@ public class CreateBoard : MonoBehaviour
     {
         
     }
+    
 }
